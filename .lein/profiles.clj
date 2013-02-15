4bfd8bf00636c6be4be4cb3af806b125eb94ebe1
Nick Bailey <nickmbailey@gmail.com>
2013-02-15 15:45:03 -0600

    Update lein

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user  {:plugins  [[lein-kibit "0.0.7"]
+                    [lein-bin "0.3.0"]
+                    [lein-pprint "1.1.1"]
+                    [lein-outdated "1.0.0"]
+                    [lein-tarsier "0.10.0"]
+                    [lein-bikeshed "0.1.0"]
+                    [jonase/eastwood "0.0.2"]]}}
