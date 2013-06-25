153cc2cfca4807b80382d9836f126402d804e83c
Nick Bailey <nickmbailey@gmail.com>
2013-06-25 13:52:48 -0500

    Updates

--- .bash_profile
+++ .bash_profile
@@ -85,5 +85,3 @@ PROMPT_COMMAND=prompt_command
 # The orginal version is saved in .bash_profile.pysave
 PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
 export PATH
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,9 @@
 {:user  {:plugins  [[lein-kibit "0.0.7"]
                     [lein-bin "0.3.0"]
+                    [lein-exec "0.3.0"]
                     [lein-pprint "1.1.1"]
                     [lein-outdated "1.0.0"]
                     [lein-tarsier "0.10.0"]
                     [lein-bikeshed "0.1.0"]
-                    [jonase/eastwood "0.0.2"]]}}
+                    [jonase/eastwood "0.0.2"]
+                    [lein-cloverage "1.0.2"]]}}
